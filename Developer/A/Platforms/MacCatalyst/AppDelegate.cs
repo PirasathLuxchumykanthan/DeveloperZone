0ed885307de98a36c120edeee12fb976a07b70c2
Pirasath <connect@object.social>
2022-07-09 22:59:30 +0200

    Come on

--- Developer/A/Platforms/MacCatalyst/AppDelegate.cs
+++ Developer/A/Platforms/MacCatalyst/AppDelegate.cs
@@ -7,9 +7,6 @@ namespace A
     public class AppDelegate : MauiUIApplicationDelegate
     {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-        public AppDelegate() :base(){
-
-            SecKeyChain.Add(new SecRecord { Service = "social.object.app.microsoft.maui.essentials.preference" });
-        }
+      
     }
 }
